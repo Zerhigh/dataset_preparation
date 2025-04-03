f96c786db5498d0665f8e3e28a0d23a475088ea0
Zerhigh <samuel@plix.at>
2025-04-03 13:10:58 +0200

    to geopackage

---
+++ to_geopackage.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import pathlib
+import pandas as pd
+import geopandas as gpd
+import shapely
+
+base = Path("C:/Users/PC/Desktop/TU/Master/MasterThesis/data/metadata")
+file = base / 's2_ortho_download_data.csv'
+
+
+def to_point(row):
+    return shapely.Point(row['lon'], row['lat'])
+
+
+def to_square(row, w=640):
+    lons = [row['geometry'].x - 640,
+            row['geometry'].x + 640,
+            row['geometry'].x + 640,
+            row['geometry'].x - 640,
+            row['geometry'].x - 640, ]
+    lats = [row['geometry'].y + 640,
+            row['geometry'].y + 640,
+            row['geometry'].y - 640,
+            row['geometry'].y - 640,
+            row['geometry'].y + 640, ]
+    return shapely.Polygon(list(zip(lons, lats)))
+
+
+data_ = pd.read_csv(file)
+data_['geometry'] = data_.apply(to_point, axis=1)
+data = gpd.GeoDataFrame(data_, crs='EPSG:4326')
+data = data.to_crs(crs=31287)
+data['geometry'] = data.apply(to_square, axis=1)
+data.to_file(base / 's2_ortho_download_data.gpkg', driver='GPKG')
+pass
